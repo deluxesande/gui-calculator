09be73ab60abb67a73b36056cf7b48be9027c5e3
deluxesande <deluxesande@gmail.com>
2023-10-01 20:23:49 +0300

    Displayed the buttons

--- calculator_c.py
+++ calculator_c.py
@@ -1,39 +1,96 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
 class Calculator:
     def __init__(self):
         self.root = tk.Tk()
 
         # Entry
         self.entry = tk.Entry(self.root, width=30, border=5)
+        self.entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
         # tk.Buttons
-        self.b1 = tk.Button(self.root, text='1', padx=40, pady=20, command=lambda: self.button_click(1))
-        self.b2 = tk.Button(self.root, text='2', padx=40, pady=20, command=lambda: self.button_click(2))
-        self.b3 = tk.Button(self.root, text='3', padx=40, pady=20, command=lambda: self.button_click(3))
-        self.b4 = tk.Button(self.root, text='4', padx=40, pady=20, command=lambda: self.button_click(4))
-        self.b5 = tk.Button(self.root, text='5', padx=40, pady=20, command=lambda: self.button_click(5))
-        self.b6 = tk.Button(self.root, text='6', padx=40, pady=20, command=lambda: self.button_click(6))
-        self.b7 = tk.Button(self.root, text='7', padx=40, pady=20, command=lambda: self.button_click(7))
-        self.b8 = tk.Button(self.root, text='8', padx=40, pady=20, command=lambda: self.button_click(8))
-        self.b9 = tk.Button(self.root, text='9', padx=40, pady=20, command=lambda: self.button_click(9))
-        self.b0 = tk.Button(self.root, text='0', padx=40, pady=20, command=lambda: self.button_click(0))
+        self.b1 = tk.Button(
+            self.root, text="1", padx=40, pady=20, command=lambda: self.button_click(1)
+        )
+        self.b2 = tk.Button(
+            self.root, text="2", padx=40, pady=20, command=lambda: self.button_click(2)
+        )
+        self.b3 = tk.Button(
+            self.root, text="3", padx=40, pady=20, command=lambda: self.button_click(3)
+        )
+        self.b4 = tk.Button(
+            self.root, text="4", padx=40, pady=20, command=lambda: self.button_click(4)
+        )
+        self.b5 = tk.Button(
+            self.root, text="5", padx=40, pady=20, command=lambda: self.button_click(5)
+        )
+        self.b6 = tk.Button(
+            self.root, text="6", padx=40, pady=20, command=lambda: self.button_click(6)
+        )
+        self.b7 = tk.Button(
+            self.root, text="7", padx=40, pady=20, command=lambda: self.button_click(7)
+        )
+        self.b8 = tk.Button(
+            self.root, text="8", padx=40, pady=20, command=lambda: self.button_click(8)
+        )
+        self.b9 = tk.Button(
+            self.root, text="9", padx=40, pady=20, command=lambda: self.button_click(9)
+        )
+        self.b0 = tk.Button(
+            self.root, text="0", padx=40, pady=20, command=lambda: self.button_click(0)
+        )
 
         # Operation buttons
-        self.b_add = tk.Button(self.root, text='+', padx=39, pady=20, command=lambda: self.add_button())
-        self.b_equal = tk.Button(self.root, text='=', padx=89, pady=20, command=self.equals_button)
-        self.b_clear = tk.Button(self.root, text='clear', padx=79, pady=20, command=self.clear_button)
+        self.b_add = tk.Button(
+            self.root, text="+", padx=39, pady=20, command=lambda: self.add_button()
+        )
+        self.b_equal = tk.Button(
+            self.root, text="=", padx=89, pady=20, command=self.equals_button
+        )
+        self.b_clear = tk.Button(
+            self.root, text="clear", padx=79, pady=20, command=self.clear_button
+        )
 
-        self.b_sub = tk.Button(self.root, text='-', padx=40, pady=20, command=lambda: self.sub_button())
-        self.b_mul = tk.Button(self.root, text='*', padx=41, pady=20, command=lambda: self.mul_button())
-        self.b_div = tk.Button(self.root, text='/', padx=41, pady=20, command=lambda: self.div_button())
+        self.b_sub = tk.Button(
+            self.root, text="-", padx=40, pady=20, command=lambda: self.sub_button()
+        )
+        self.b_mul = tk.Button(
+            self.root, text="*", padx=41, pady=20, command=lambda: self.mul_button()
+        )
+        self.b_div = tk.Button(
+            self.root, text="/", padx=41, pady=20, command=lambda: self.div_button()
+        )
+
+        # Displaying the buttons
+        self.b1.grid(row=3, column=0)
+        self.b2.grid(row=3, column=1)
+        self.b3.grid(row=3, column=2)
+
+        self.b4.grid(row=2, column=0)
+        self.b5.grid(row=2, column=1)
+        self.b6.grid(row=2, column=2)
+
+        self.b7.grid(row=1, column=0)
+        self.b8.grid(row=1, column=1)
+        self.b9.grid(row=1, column=2)
+
+        self.b0.grid(row=4, column=0)
+
+        self.b_clear.grid(row=5, column=1, columnspan=2)  # Clear
+        self.b_add.grid(row=5, column=0)  # Add
+        self.b_equal.grid(row=6, column=1, columnspan=2)  # Answer
+
+        self.b_sub.grid(row=6, column=0)
+        self.b_mul.grid(row=4, column=1)
+        self.b_div.grid(row=4, column=2)
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
         self.root.mainloop()
-    
+
     def on_close(self):
-        if messagebox.askyesno(title='Quit?', message="Are you sure?"):
+        if messagebox.askyesno(title="Quit?", message="Are you sure?"):
             self.root.destroy()
 
     def button_click(self, number):
@@ -44,7 +101,7 @@ class Calculator:
     def add_button(self):
         first_num = self.entry.get()
         global f_num, math
-        math = 'addition'
+        math = "addition"
 
         try:
             f_num = int(first_num)
@@ -56,7 +113,7 @@ class Calculator:
     def sub_button(self):
         first_num = self.entry.get()
         global f_num, math
-        math = 'subtraction'
+        math = "subtraction"
 
         try:
             f_num = int(first_num)
@@ -68,7 +125,7 @@ class Calculator:
     def div_button(self):
         first_num = self.entry.get()
         global f_num, math
-        math = 'division'
+        math = "division"
 
         try:
             f_num = int(first_num)
@@ -80,7 +137,7 @@ class Calculator:
     def mul_button(self):
         first_num = self.entry.get()
         global f_num, math
-        math = 'multiplacation'
+        math = "multiplacation"
 
         try:
             f_num = int(first_num)
@@ -90,18 +147,18 @@ class Calculator:
         self.entry.delete(0, tk.END)
 
     def equals_button(self):
-        if self.entry.get() != '':
+        if self.entry.get() != "":
             second_num = self.entry.get()
             self.entry.delete(0, tk.END)
 
             try:
-                if math == 'addition':
+                if math == "addition":
                     self.entry.insert(0, f_num + int(second_num))
-                if math == 'subtraction':
+                if math == "subtraction":
                     self.entry.insert(0, f_num - int(second_num))
-                if math == 'division':
+                if math == "division":
                     self.entry.insert(0, f_num / int(second_num))
-                if math == 'multiplacation':
+                if math == "multiplacation":
                     self.entry.insert(0, f_num * int(second_num))
             except NameError:
                 pass
@@ -110,6 +167,5 @@ class Calculator:
         self.entry.delete(0, tk.END)
 
 
-
-if __name__== '__main__':
+if __name__ == "__main__":
     Calculator()
